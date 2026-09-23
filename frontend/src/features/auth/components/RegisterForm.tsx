f3015d6224d858887f40a36f2ee06c05a2642c95
Safuvank <safuvanvkm@gmail.com>
2026-09-23 15:49:39 +0530

    registration form ui changed

--- frontend/src/features/auth/components/RegisterForm.tsx
+++ frontend/src/features/auth/components/RegisterForm.tsx
@@ -101,7 +101,6 @@ export default function RegisterForm() {
                   stroke="currentColor"
                   strokeWidth="4"
                 />
-
                 <path
                   className="opacity-75"
                   fill="currentColor"
@@ -117,17 +116,14 @@ export default function RegisterForm() {
                   d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
                   fill="#4285F4"
                 />
-
                 <path
                   d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
                   fill="#34A853"
                 />
-
                 <path
                   d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
                   fill="#FBBC05"
                 />
-
                 <path
                   d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
                   fill="#EA4335"
@@ -319,6 +315,7 @@ export default function RegisterForm() {
 
           <div className="relative">
             <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3.5">
+              {/* Refined Key Icon for Password */}
               <svg
                 className="h-5 w-5 text-gray-400"
                 fill="none"
@@ -329,7 +326,7 @@ export default function RegisterForm() {
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v6.75a2.25 2.25 0 002.25-2.25V6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
+                  d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z"
                 />
               </svg>
             </div>
@@ -376,7 +373,6 @@ export default function RegisterForm() {
                     strokeLinejoin="round"
                     d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                   />
-
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
@@ -402,6 +398,7 @@ export default function RegisterForm() {
 
           <div className="relative">
             <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3.5">
+              {/* Secure Shield-Check Icon for Confirm Password */}
               <svg
                 className="h-5 w-5 text-gray-400"
                 fill="none"
@@ -412,7 +409,7 @@ export default function RegisterForm() {
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
+                  d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z"
                 />
               </svg>
             </div>
@@ -463,7 +460,6 @@ export default function RegisterForm() {
                     strokeLinejoin="round"
                     d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                   />
-
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
